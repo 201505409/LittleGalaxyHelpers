1d8b99543be7f8f541a95a0ef5ad847b2acae95b
201505409 <17643083+201505409@users.noreply.github.com>
2025-04-24 10:06:17 +0200

    Update GenEngineAttribWatchWindow.sql
    Fixed it

--- GenEngineAttribWatchWindow.sql
+++ GenEngineAttribWatchWindow.sql
@@ -1,11 +1,11 @@
 --Copy and Paste the Results of this script into a text file
 --and load that file into object viewer
 
-Declare @rootAppEngId int
+DECLARE @rootAppEngId int
 
 CREATE TABLE #Attribs(attrib varchar(150))
 
-INSERT  INTO #Attribs (attrib) VALUES 
+INSERT INTO #Attribs (attrib) VALUES 
 ------------------------------------------------
 --Configure Required Attributes below
 ------------------------------------------------
@@ -16,20 +16,20 @@ INSERT  INTO #Attribs (attrib) VALUES
 ------------------------------------------------
 SELECT @rootAppEngId = [template_definition_id]
   FROM [dbo].[template_definition]
-  Where original_template_tagname = '$AppEngine'
+  WHERE original_template_tagname = '$AppEngine'
 
 SELECT(
 SELECT 'Engine Audit' as "Tab", (
-  SELECT t1.[hierarchical_name] + t2.attrib as ReferenceString
+  SELECT t1.[hierarchical_name] + t2.attrib COLLATE SQL_Latin1_General_CP1_CI_AS as ReferenceString
   FROM [dbo].[gobject] as t1
   JOIN #Attribs as t2
-  on 1 = 1
-  where t1.template_definition_id = @rootAppEngId 
+  ON 1 = 1
+  WHERE t1.template_definition_id = @rootAppEngId 
   AND t1.[is_template] = 0
   AND t1.deployed_package_id <> 0
   ORDER BY ReferenceString
   FOR XML PATH(''), type
  ) FOR XML RAW('Watch'), type
  ) FOR XML RAW('VisualLMXTestTool')
- 
+
 DROP TABLE #Attribs
